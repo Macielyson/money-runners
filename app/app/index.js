bf39cdae5163754ae10eca1f25e8c3387c12764e
Macielyson Nascimento <macielyson.s@gmail.com>
2024-08-10 18:09:02 -0300

    iniciando o front

---
+++ app/app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Tour from '../src/pages/Tour';
+
+const App = () => {
+    return (
+        <Tour />
+    )
+}
+
+export default App;
---
+++ app/src/pages/Tour/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Tour = () => {
+    return (
+        <View>
+            <Text>Olá Mundo</Text>
+        </View>
+    )
+}
+
+export default Tour;
